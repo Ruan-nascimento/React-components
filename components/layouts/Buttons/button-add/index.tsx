41c1f29352eda7e664a181e1cfe20db4d214882e
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-04-18 22:07:15 -0300

    datails

--- components/layouts/Buttons/button-add/index.tsx
+++ components/layouts/Buttons/button-add/index.tsx
@@ -1,13 +1,16 @@
 import { HTMLAttributes, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface ButtonAddProps extends HTMLAttributes<HTMLButtonElement> {
     children: ReactNode
+    className?: string
 }
 
-export const ButtonAdd = ({children}: ButtonAddProps) => {
+export const ButtonAdd = ({children, ...rest}: ButtonAddProps) => {
     return(
         <button
-        className="p-2 px-4 rounded-lg bg-green-500 cursor-pointer text-white duration-200 ease-in-out hover:bg-green-500/80 active:bg-green-500/60"
+        {...rest}
+    className={twMerge(`p-2 px-4 rounded-lg bg-green-500 cursor-pointer text-white duration-200 ease-in-out hover:bg-green-500/80 active:bg-green-500/60`, rest.className)}
         >
             {children}
         </button>
--- components/layouts/Buttons/button-remove/index.tsx
+++ components/layouts/Buttons/button-remove/index.tsx
@@ -1,13 +1,16 @@
 import { HTMLAttributes, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface ButtonRemoveProps extends HTMLAttributes<HTMLButtonElement> {
     children: ReactNode
+    className?: string
 }
 
-export const ButtonRemove = ({children}: ButtonRemoveProps) => {
+export const ButtonRemove = ({children, ...rest}: ButtonRemoveProps) => {
     return(
         <button
-        className="p-2 px-4 rounded-lg bg-red-500 cursor-pointer text-white duration-200 ease-in-out hover:bg-red-500/80 active:bg-red-500/60"
+        {...rest}
+        className={twMerge("p-2 px-4 rounded-lg bg-red-500 cursor-pointer text-white duration-200 ease-in-out hover:bg-red-500/80 active:bg-red-500/60", rest.className)}
         >
             {children}
         </button>
